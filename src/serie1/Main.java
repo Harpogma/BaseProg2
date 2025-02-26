7b9ed04a96d1750d5ca1b60239784c43d4271233
Harpogma <gd.temp@pm.me>
2025-02-26 13:30:58 +0100

    First commit

---
+++ src/serie1/Main.java
@@ -0,0 +1,17 @@
+package serie1;
+
+public class Main {
+    public static int sumArray(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int result = sumArray(array);
+        System.out.println(result);
+    }
+}
